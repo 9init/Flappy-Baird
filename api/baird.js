494511ebb2ac8f59d7de4c1f5bea8bb148a35fb6
9init <emovent2e@gmail.com>
2020-10-14 01:09:02 +0200

    Brain was added

--- api/baird.js
+++ api/baird.js
@@ -2,7 +2,47 @@ class Baird{
   constructor() {
     this.position = createVector(80, height/2)
     this.gravity = 1;
-    this.velocity = 1;
+    this.velocity = 0;
+    this.brain = new NeuralNetwork(8, 20, 2);
+    this.score = 0;
+    this.fitness = 0;
+  }
+
+  think(wall){
+
+    let closest = null;
+    let closestD = Infinity;
+    for (let i = 0; i < wall.walls.length; i++){
+      let d = wall.walls[i].up.x - this.position.x;
+      if(d < closestD && d > 0){
+        closest = wall.walls[i];
+        closestD = d;
+      }
+    }
+
+    let velocity = this.velocity;
+    let position = this.position.y;
+    let top_y_End = closest.up.y / height;
+    let top_y_Width_start = closest.up.x / width;
+    let top_y_Width_end = (closest.up.x + wall.wall_size) / width;
+    let bot_y_End = closest.down.y / height;
+    let bot_y_Width_start = closest.down.x / width;
+    let bot_y_Width_end = (closest.down.x + wall.wall_size) / width;
+
+    let inputs= [
+      velocity,
+      position,
+      top_y_End,
+      top_y_Width_start,
+      top_y_Width_end,
+      bot_y_End,
+      bot_y_Width_start,
+      bot_y_Width_end
+    ];
+
+    let output = this.brain.predict(inputs)
+    if(output[0] > output[1])
+      this.jump();
   }
 
   jump(){
@@ -10,18 +50,19 @@ class Baird{
   }
 
   update(){
-      this.velocity += this.gravity
-      this.velocity *= 0.9;
+    this.score++;
+    this.velocity += this.gravity
+    this.velocity *= 0.9;
 
-      if(this.position.y > height){
-        this.position.y = height;
-        this.velocity = 0;
-      }
-      if(this.position.y < 0){
-        this.position.y = 0;
-        this.velocity = 0;
-      }
-      this.position.y += this.velocity
+    if(this.position.y > height){
+      this.position.y = height;
+      this.velocity = 0;
+    }
+    if(this.position.y < 0){
+      this.position.y = 0;
+      this.velocity = 0;
+    }
+    this.position.y += this.velocity
   }
 
   highLight(){
