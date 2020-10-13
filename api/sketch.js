532346cb128b6de44ba96623fafc5d7b9bc0d9df
9init <emovent2e@gmail.com>
2020-10-13 21:22:41 +0200

    jump key changed from Mouse to Space Bar

--- api/sketch.js
+++ api/sketch.js
@@ -1,22 +1,21 @@
 let baird;
 let wall;
-
 function setup(){
   createCanvas(1000, 500);
-  wall = new Wall();
   baird = new Baird();
   wall = new Wall();
   frameRate(60);
 }
 
-function mousePressed(){
-  baird.jump();
-
+function keyPressed(){
+  if (keyCode == 32)
+    baird.jump();
 }
 
 function draw(){
   background(64,224,208);
   baird.update();
+  baird.show();
   wall.update();
-
+  wall.hit(baird);
 }
