a34fda888364ce1eb3f3e8a19cebbbaee6e8ae40
9init <emovent2e@gmail.com>
2020-10-14 01:08:13 +0200

    make it suitable for the GA

--- api/sketch.js
+++ api/sketch.js
@@ -1,21 +1,32 @@
-let baird;
+const POPULATION = 100;
+let bairds = [];
 let wall;
 function setup(){
   createCanvas(1000, 500);
-  baird = new Baird();
+  for(let i = 0; i < POPULATION; i++){
+    bairds[i] = new Baird();
+  }
   wall = new Wall();
   frameRate(60);
 }
 
-function keyPressed(){
-  if (keyCode == 32)
-    baird.jump();
-}
+// function keyPressed(){
+//   if (keyCode == 32)
+//     baird.jump();
+// }
 
 function draw(){
   background(64,224,208);
-  baird.update();
-  baird.show();
   wall.update();
-  wall.hit(baird);
+  for(let i = 0; i < bairds.length; i++){
+    let baird = bairds[i];
+    baird.think(wall);
+    baird.update();
+    baird.show();
+    if(wall.hit(baird))
+      bairds.splice(i, 1);
+  }
+  
+
+  
 }
